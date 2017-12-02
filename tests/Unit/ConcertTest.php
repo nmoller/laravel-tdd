2e188f4f6c3577340f7e20659fb102d6b90c65ed
Nelson Moller <nmoller.c@gmail.com>
2017-12-02 16:15:24 -0500

    Refactoring pour enlever logique de la presentation et reformulation de tests pour augmeter las vitesse.

--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ConcertTest extends TestCase
 {
-    use DatabaseMigrations;
+    //use DatabaseMigrations;
 
     /**
      * @test
@@ -19,12 +19,32 @@ class ConcertTest extends TestCase
      */
     public function can_get_formatted_date()
     {
-        $concert = factory(Concert::class)->create([
+        $concert = factory(Concert::class)->make([
           'date' => Carbon::parse('2017-12-01 8:00pm', 'America/Toronto')
         ]);
 
-        $date = $concert->formatted_date;
+        $this->assertEquals('December 1, 2017', $concert->formatted_date);
+    }
 
-        $this->assertEquals('December 1, 2017', $date);
+    /**
+     * @test
+     */
+    public function can_get_formatted_start_time(){
+        $concert = factory(Concert::class)->make([
+            'date' => Carbon::parse('2017-12-01 17:00:00')
+        ]);
+
+        $this->assertEquals( '5:00pm' , $concert->formatted_start_time);
+    }
+
+    /**
+     * @test
+     */
+    public function can_get_ticket_price_in_dollars() {
+        $concert = factory(Concert::class)->make([
+            'ticket_price' => 3450
+        ]);
+
+        $this->assertEquals( '34.50' , $concert->ticket_price_in_dollars);
     }
 }
