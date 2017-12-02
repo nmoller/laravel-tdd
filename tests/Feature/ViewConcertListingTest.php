9b7f193d36801c49be7c8aafee2c6be674e70861
Nelson Moller <nmoller.c@gmail.com>
2017-12-02 16:20:45 -0500

    Add missing fields to be sure the view is OK.

--- tests/Feature/ViewConcertListingTest.php
+++ tests/Feature/ViewConcertListingTest.php
@@ -25,18 +25,29 @@ class ViewConcertListingTest extends TestCase
         $concert = factory(Concert::class)->create([
             'title' => 'The Red Chord',
             'subtitle' => 'with Animosity and Lethargy',
-            'date' => Carbon::parse('December 13, 2017 8:00pm', 'America/Toronto'),
-            'additional_information' => 'For tickets, call (555) 555-5555.'
+            'date' => Carbon::parse('December 13, 2017 8:00pm'),
+            'ticket_price' => 3250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+            'additional_information' => 'For tickets, call (555) 555-5555.',
         ]);
 
         // Act
             // View the concert listing
         $response = $this->get('/concerts/'.$concert->id);
 
-        //Assert
-            // See the concert details
+        $response->assertStatus(200);
         $response->assertSee('The Red Chord');
         $response->assertSee('with Animosity and Lethargy');
+        $response->assertSee('December 13, 2017');
+        $response->assertSee('8:00pm');
+        $response->assertSee('32.50');
+        $response->assertSee('The Mosh Pit');
+        $response->assertSee('123 Example Lane');
+        $response->assertSee('Laraville, ON 17916');
         $response->assertSee('For tickets, call (555) 555-5555.');
     }
 }
